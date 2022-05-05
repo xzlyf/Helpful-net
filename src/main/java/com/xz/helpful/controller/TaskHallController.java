aa794478a8814f9559d471cd5facec7a77cc8ee3
xzlyf <czr2001@outlook.com>
2022-05-05 20:45:49 +0800

    任务大厅

--- src/main/java/com/xz/helpful/controller/TaskHallController.java
+++ src/main/java/com/xz/helpful/controller/TaskHallController.java
@@ -2,21 +2,15 @@ package com.xz.helpful.controller;
 
 import com.xz.helpful.global.RedisKey;
 import com.xz.helpful.pojo.Task;
-import com.xz.helpful.pojo.vo.BaseVo;
 import com.xz.helpful.service.TaskService;
 import com.xz.helpful.service.UserServer;
 import com.xz.helpful.utils.RedisUtil;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * 任务大厅接口
@@ -34,13 +28,12 @@ public class TaskHallController {
     @RequestMapping("/get")
     public ModelAndView getRandom() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("view/task");
         Subject subject = SecurityUtils.getSubject();
         String email = subject.getPrincipal().toString();
         if (email == null) {
             //todo重定向login页面
+            modelAndView.setViewName("view/task-error");
             modelAndView.addObject("msg","未登录");
-            modelAndView.addObject("code",5);
             return modelAndView;
         }
         //1、从redis根据email取出userID
@@ -49,9 +42,8 @@ public class TaskHallController {
         if (userId == null) {
             userId = userServer.findUserIdByEmail(email);
             if (userId == null) {
-                //todo 手动跳转404页面
+                modelAndView.setViewName("view/task-error");
                 modelAndView.addObject("msg","非法请求");
-                modelAndView.addObject("code",2);
                 return modelAndView;
             }
         }
@@ -59,6 +51,7 @@ public class TaskHallController {
         //3.开始取任务
         Task one = taskService.getOne(userId);
         //4.使用view页面返回html
+        modelAndView.setViewName("view/task");
         modelAndView.addObject("task",one);
         return modelAndView;
     }
