6de418527572bd8429bfa94717d13dde5c346f7d
xzlyf <czr2001@outlook.com>
2022-05-13 21:20:18 +0800

    bili api 视频信息抓取

--- src/main/java/com/xz/helpful/controller/TaskController.java
+++ src/main/java/com/xz/helpful/controller/TaskController.java
@@ -1,15 +1,19 @@
 package com.xz.helpful.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.xz.helpful.annotation.LimitRequest;
+import com.xz.helpful.pojo.BiliMovie;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.IOException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 
 /**
@@ -21,27 +25,43 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/taskManager")
 public class TaskController {
+    public static final String BILI_MOVIE_INFO = "http://api.bilibili.com/x/web-interface/view?bvid=";
+    @Autowired
+    private RestTemplate restTemplate;
 
     /**
      * bilibili api 获取视频信息 ：http://api.bilibili.com/x/web-interface/view?bvid=
      *
      * @param bv bvid号
      */
-    @ResponseBody
     @GetMapping("/checkBV")
+    @ResponseBody
+    @LimitRequest(count = 8,time = 60000)
     public Object checkBV(@RequestParam String bv) {
-        Document document = null;
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("view/task-error");
+
+        ResponseEntity<String> responseEntity;
         try {
-            document =
-                    Jsoup.connect("http://api.bilibili.com/x/web-interface/view?bvid=" + bv)
-                            .userAgent("Mozilla/5.0 (Windows NT 5.1; zh-CN) AppleWebKit/535.12 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/535.12")
-                            .ignoreContentType(true)
-                            .timeout(3000)
-                            .get();
-        } catch (IOException e) {
+            responseEntity = restTemplate.getForEntity(BILI_MOVIE_INFO + bv, String.class);
+        } catch (Exception e) {
             e.printStackTrace();
-            return null;
+            modelAndView.addObject("msg", "数据源请求失败，请稍后重试！");
+            return modelAndView;
         }
-        return document.body().text();
+        String body = responseEntity.getBody();
+        JSONObject obj = JSON.parseObject(body);
+        if (obj == null || obj.getInteger("code") != 0) {
+            modelAndView.addObject("msg", "没有找到视频号，请检查！");
+            return modelAndView;
+        }
+        BiliMovie data = obj.getObject("data", BiliMovie.class);
+        if (data == null) {
+            modelAndView.addObject("msg", "数据源解析失败！");
+            return modelAndView;
+        }
+        modelAndView.setViewName("view/task-check");
+        modelAndView.addObject("info", data);
+        return modelAndView;
     }
 }
---
+++ src/main/java/com/xz/helpful/pojo/BiliMovie.java
@@ -0,0 +1,18 @@
+package com.xz.helpful.pojo;
+
+import lombok.Data;
+
+/**
+ * @author XiaoZe
+ * @email czr2001@outlook.com
+ * @date 2022/5/13 20:09
+ */
+@Data
+public class BiliMovie {
+    private String bvid;
+    private Long aid;
+    private String pic;
+    private String title;
+    private String tname;
+    private Long ctime;
+}
